b409bfe83e76a46dc7b2237043255b36fdbde5cb
Scott Claessens <scla896@UoA.auckland.ac.nz>
2023-10-31 12:00:18 +1300

    Create report

--- _targets.R
+++ _targets.R
@@ -5,7 +5,7 @@ source("R/functions_data.R")
 source("R/functions_plots.R")
 source("R/functions_modelling.R")
 options(tidyverse.quiet = TRUE)
-tar_option_set(packages = c("lavaan", "tidyverse"))
+tar_option_set(packages = c("lavaan", "papaja", "tidyverse"))
 # workflow
 list(
   # files
@@ -24,6 +24,8 @@ list(
   # fit random intercept cross-lagged panel model with controls
   tar_target(riclpm2.1, fitRICLPM2(d, constrained = TRUE)),
   tar_target(riclpm2.2, fitRICLPM2(d, constrained = FALSE)),
+  # knit report
+  tar_render(report, "report.Rmd"),
   # print session info for reproducibility
   tar_target(sessionInfo, writeLines(capture.output(sessionInfo()), "sessionInfo.txt"))
 )
